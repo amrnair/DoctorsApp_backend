b1b88c5816a9649952f1d695744eccc151d70d90
Amrutha <amruthasnair0502@gmail.com>
2024-06-21 17:19:43 +0530

    commit message 2

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,8 +13,8 @@ const otpModel = require('../models/otp')
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'otp788935@gmail.com', // this is the email that will be used to send the email.
-        pass: 'lofu tmez fnwi lhoh', //this is the password that is created in app passwords in gmail setting options.
+        user: 'amruthasnair0502@gmail.com', // this is the email that will be used to send the email.
+        pass: 'yjwl rvty pvtj nuoa', //this is the password that is created in app passwords in gmail setting options.
     },
 });
 
